e9bffe2b0fd13caec3e5fc8e8ce86054c953f601
cembosaurus <milos.bencek@gmail.com>
2019-03-06 20:08:45 +1100

    06032019

--- DataStore.UnitTest/Controllers/LockersController_UnitTest.cs
+++ DataStore.UnitTest/Controllers/LockersController_UnitTest.cs
@@ -1,11 +1,15 @@
-﻿using DataStore.Data;
+﻿using DataStore.Controllers;
+using DataStore.Data;
 using DataStore.DTOs;
 using DataStore.Models;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace DataStore.UnitTest.Data
 {
@@ -14,17 +18,26 @@ namespace DataStore.UnitTest.Data
     class LockersController_UnitTest
     {
 
+        private Mock<LockersDTO> _lockers;
+
         [SetUp]
         public void SetUp()
         {
-
+            _lockers = new Mock<LockersDTO>();
         }
 
         [Test]
         public void Get_test()
         {
+            var repo = new Mock<ILockersRepository>();
+            repo.Setup(s => s.Lockers()).ReturnsAsync(_lockers.Object);
+            var controller = new LockersController(repo.Object);
 
-
+            var result = controller.Get();
+            var data = result.Result;
+           
+            Assert.IsInstanceOf<Task<IActionResult>>(result);
+            Assert.That(data, Is.InstanceOf<OkObjectResult>());
         }
 
     }
